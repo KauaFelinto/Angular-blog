6b42cd0b5c2eb475196e1337ce7e6d3f2a6c09f8
KauaFelinto <kauafelintoc@gmail.com>
2023-10-03 00:35:26 -0300

    content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+{
+    "id":"1",
+    "photo":"aaa",
+    "title": "Aranhaverso 2 ganha data para chegar às plataformas digitais",
+    "description": "Depois de se reunir com Gwen Stacy, Homem-Aranha é jogado no multiverso, onde ele encontra uma equipe encarregada de proteger sua própria existência.",
+    "photoCover": "https://meups.com.br/wp-content/uploads/2023/04/Marvels-Spider-Man-em-Atraves-do-Aranhaverso.jpg"
+},
+{
+    "id":"2",
+    "photo":"aaa",
+    "title": "Besouro Azul tem mesmo um futuro no novo DCU?",
+    "description": "O adolescente Jaime Reyes ganha superpoderes quando um misterioso escaravelho se prende à sua coluna e lhe fornece uma poderosa armadura alienígena azul.",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/review-besouro-azul_uuae.jpg"
+},
+{
+    "id":"3",
+    "photo":"aaa",
+    "title": "Data da 2ª temporada de Loki é confirmada pela Marvel",
+    "description": "O adolescente Jaime Reyes ganha superpoderes quando um misterioso escaravelho se prende à sua coluna e lhe fornece uma poderosa armadura alienígena azul.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2022/07/Loki-Disney-Plus.jpg"
+},
+{
+    "id":"4",
+    "photo":"aaa",
+    "title": "Porque a taxação é tão ruim? O que não te contam",
+    "description": "O adolescente Jaime Reyes ganha superpoderes quando um misterioso escaravelho se prende à sua coluna e lhe fornece uma poderosa armadura alienígena azul.",
+    "photoCover": "https://revistaanamaria.com.br/media/_versions/2023-08/shein-ali-express-shopee-mudancas_widelg.jpg"
+}
+]
